8abea22b17d9ef2a88e66640331136c44d4f0fab
unifiedsaad <saad.m@whyunified.com>
2020-04-10 18:25:42 +0500

    initial

--- main/settings.py
+++ main/settings.py
@@ -118,12 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = (
-    os.path.join(BASE_DIR, 'static_root')
-)
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 MEDIA_URL = '/media/'
 ORGS_SLUGFIELD = 'autoslug.fields.AutoSlugField'
